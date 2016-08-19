92627cf75e04b5dc0a45a8b048cb2c035bd12c33
Adam Luchini <aluchini@gmail.com>
2016-08-19 15:41:20 -0700

    add front page styling with headers

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -3,11 +3,8 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   timesAnswered: Ember.computed('question.answers', function() {
     var numberTimesAnswered = this.get('question.answers').get('length');
-    // console.log(.get('length'))
-    if (numberTimesAnswered <= 1) {
+    if (numberTimesAnswered === 0) {
       return ' nobody has answered this yet';
-    // } else if (length === 0) {
-    //   return "0 answers";
     }
     else {
       return ' answers submitted: ' + numberTimesAnswered;
