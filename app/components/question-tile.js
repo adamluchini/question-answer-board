385ba8022260849c67445d140ff6f2d5f00e014b
Adam Luchini <aluchini@gmail.com>
2016-08-19 12:22:54 -0700

    shows if a question has been answered and how many replies

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,15 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  answerLength: Ember.computed('question.answers', function() {
-    var length = this.get('question.answers').get('length');
-    if (length === 1) {
-      return length + ' answer';
-    } else if (length === 0) {
-      return "0 answers";
+  timesAnswered: Ember.computed('question.answers', function() {
+    var numberTimesAnswered = this.get('question.answers').get('length');
+    // console.log(.get('length'))
+    if (numberTimesAnswered <= 1) {
+      return ' nobody has answered this yet';
+    // } else if (length === 0) {
+    //   return "0 answers";
     }
     else {
-      return length + ' answers';
+      return ' answers submitted: ' + numberTimesAnswered;
     }
   })
 
