d06b49721a3dff2d4332316edba96cd81ac5efed
Adam Luchini <aluchini@gmail.com>
2016-08-26 10:14:27 -0700

    add 3 fire glyphs when comments are greater than 5

--- app/helpers/hot-topic.js
+++ app/helpers/hot-topic.js
@@ -3,8 +3,10 @@ import Ember from 'ember';
 export function hotTopic(params) {
   var question = params[0];
 
-  if(question.get('answers').get('length') > 2) {
-    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-fire"></span>');
+  if(question.get('answers').get('length') > 5) {
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-fire"></span> <span class="glyphicon glyphicon-fire"></span> <span class="glyphicon glyphicon-fire"></span>');
+  } else if(question.get('answers').get('length')> 2) {
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-fire"></span>')
   } else {
     return Ember.String.htmlSafe('<span class="glyphicon glyphicon-ice-lolly"></span>');
   }
