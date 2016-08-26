537b88f2298fc3ec933bdf082d8a6e4500ab252f
Adam Luchini <aluchini@gmail.com>
2016-08-26 09:08:29 -0700

    add favorites page and push favorite answers to a page

--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -1,5 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  
+  favorites: Ember.inject.service(),
+
+  actions: {
+  addToFavorites(answer) {
+    this.get('favorites').add(answer);
+    },
+  }
 });
